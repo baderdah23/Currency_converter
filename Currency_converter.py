0829200bfcb9f531193aa46cd57550d827e01807
baderdah23 <bd1911.k5@gmail.com>
2025-12-17 19:03:29 +0200

    Currency_converter

---
+++ Currency_converter.py
@@ -0,0 +1,30 @@
+import requests 
+ 
+initial_currency = input("enter initial currency: ").upper()
+target_currency = input("enter target currency: ").upper()
+ 
+while(True): 
+    try: 
+        amount = float(input("enter the amount : ")) 
+        if amount<=0: 
+            raise ValueError 
+        break
+    except ValueError: 
+        print ("Amount must be a number greater than zero!") 
+        break
+ 
+url = f"https://api.apilayer.com/fixer/convert?to={target_currency}&from={initial_currency}&amount={amount}" 
+ 
+payload = {} 
+headers= { 
+  "apikey": "dNQ4HPsfvjFhTTCkN8A0C8MJr4TmmnH8" 
+} 
+ 
+response = requests.request("GET", url, headers=headers, data = payload) 
+if response.status_code != 200: 
+    print("api error") 
+    quit() 
+ 
+status_code = response.status_code 
+result = response.json() 
+print(f"{amount} {initial_currency} : {result['result']} {target_currency}")
